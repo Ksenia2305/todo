3972d9e1e10caa1a40419c40b136d86d23acd5fc
k.sidelnikova <k.sidelnikova@qiwi.com>
2024-02-05 02:28:55 +0300

    Add HW4

--- src/test/kotlin/Testdata.kt
+++ src/test/kotlin/Testdata.kt
@@ -3,10 +3,9 @@ import data.Task
 import kotlin.random.Random.Default.nextInt
 
 fun generateTask(priority: Priority, completed: Boolean = false, numberOfTask: Int): List<Task> {
-    val taskId = nextInt(1,1000)
-    var listTasks = listOf<Task>()
+    var listTasks = mutableListOf<Task>()
     for (i in 1..numberOfTask) {
-        listTasks = listOf(Task(id = taskId, name = "myTask$taskId", priority = priority, completed = completed))
+        listTasks.add((i - 1), Task(id = i, name = "myTask$i", priority = priority, completed = completed))
     }
     return listTasks
 }
